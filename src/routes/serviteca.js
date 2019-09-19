b2f0690ef8026a9c9ef5fb83a7d38b88ca03fa4f
Isaza156 <jonathan.isaza@hotmail.com>
2019-09-18 23:51:31 -0500

    act

--- src/routes/serviteca.js
+++ src/routes/serviteca.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const _ = require('underscore'); 
+const _ = require('underscore');
 const serviteca = require('../sample.json');
 
 router.post('/', (req, res) => {
@@ -10,13 +10,13 @@ router.post('/', (req, res) => {
         if (Hora < 8 || Hora > 18) {
             res.send('Horario Incorrecto');
         } else {
-            _.each(serviteca, (servi) => {         
+            _.each(serviteca, (servi) => {
                 if (Dia == servi.Dia && Mes == servi.Mes && Hora == servi.Hora) {
                     res.send('No tenemos Horario o Fecha Disponible');
                 } else {
                     const id = serviteca.length + 1;
                     const newServi = { ...req.body, id } // con los 3 puntos ... pasa todo el objeto req.body dentro de un nuevo objeto, asi podremos guardar       
-                    serviteca.push(newServi.saved());                   
+                    serviteca.push(newServi);
                     res.json(serviteca);
                 }
             });
